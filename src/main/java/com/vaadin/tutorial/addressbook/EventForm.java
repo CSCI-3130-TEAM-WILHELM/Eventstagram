81e8d0f2843c14c7c6049e967f29717d1e2e68d8
Jen-Cooper <jen.cooper2014@gmail.com>
2017-03-03 23:55:33 -0400

    Added an example for what should be shown when an event is selected

--- src/main/java/com/vaadin/tutorial/addressbook/EventForm.java
+++ src/main/java/com/vaadin/tutorial/addressbook/EventForm.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.addressbook;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.tutorial.addressbook.backend.Contact;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
@@ -25,12 +26,16 @@ import com.vaadin.v7.ui.TextField;
 //CHANGES 2
 
 public class EventForm extends FormLayout {
-
-    Button save = new Button("Save", this::save);
-    Button cancel = new Button("Cancel", this::cancel);
-    TextField event = new TextField("Event");
-
     Contact contact;
+    private Label eventNameLabel = new Label(); 
+    private Label eventDescriptionLabel = new Label();
+    private Label eventDateLabel = new Label();
+    private Label eventLocationLabel = new Label();
+    private Label headCountLabel = new Label();
+    
+    private Button attendingButton = new Button("Attending");
+    private Button interestedButton = new Button("Interested");
+    		
 
     // Easily bind forms to beans and manage validation and buffering
     BeanFieldGroup<Contact> formFieldBindings;
@@ -47,19 +52,22 @@ public class EventForm extends FormLayout {
          * With Vaadin built-in styles you can highlight the primary save button
          * and give it a keyboard shortcut for a better UX.
          */
-        save.setStyleName(ValoTheme.BUTTON_PRIMARY);
-        save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+    	attendingButton.addClickListener(e -> attendingEvent());
+    	interestedButton.addClickListener(e -> interestedEvent());
+    	
+    	eventNameLabel.setValue("Event Name");
+    	eventDescriptionLabel.setValue("A real fun time");
+    	eventDateLabel.setValue("February 29th 2017 at 7:00pm");
+    	eventLocationLabel.setValue("Dalhousie University");
         setVisible(false);
     }
 
     private void buildLayout() {
         setSizeUndefined();
         setMargin(true);
-
-        HorizontalLayout actions = new HorizontalLayout(save, cancel);
+        HorizontalLayout actions = new HorizontalLayout(attendingButton, interestedButton);
         actions.setSpacing(true);
-
-        addComponents(actions, event);
+        addComponents(actions, eventNameLabel, eventDateLabel, eventLocationLabel, eventDescriptionLabel);
     }
 
     /*
@@ -73,6 +81,10 @@ public class EventForm extends FormLayout {
      * classes and receive to various Vaadin component events, like button
      * clicks. Or keep it simple and compact with Lambda expressions.
      */
+    
+    
+    
+    /*
     public void save(Button.ClickEvent event) {
         try {
             // Commit the fields from UI to DAO
@@ -94,14 +106,24 @@ public class EventForm extends FormLayout {
         Notification.show("Cancelled", Type.TRAY_NOTIFICATION);
         getUI().contactList.select(null);
     }
-
+*/ 
+    private void attendingEvent()
+    {
+    	//method to update user's attendance
+    }
+    
+    private void interestedEvent()
+    {
+    	//method to update user's interestedness
+    }
+    
     void edit(Contact contact) {
         this.contact = contact;
         if (contact != null) {
             // Bind the properties of the contact POJO to fields in this form
             formFieldBindings = BeanFieldGroup.bindFieldsBuffered(contact,
                     this);
-            event.focus();
+            //event.focus();
         }
         setVisible(contact != null);
     }
