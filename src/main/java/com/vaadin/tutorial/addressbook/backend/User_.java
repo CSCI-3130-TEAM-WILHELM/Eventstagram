146cb43c6114cb2663826071e7cbc7a4e5cd9cd7
Greydesk <rod.naugler@gmail.com>
2017-03-19 19:25:13 -0300

    Create new UserStory1 from Userstory8 to complete userstory1
    With Darius. Master branch has fault, using userstory8 as base

---
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/User_.java
@@ -0,0 +1,13 @@
+package com.vaadin.tutorial.addressbook.backend;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2017-03-19T18:48:06.558-0300")
+@StaticMetamodel(User.class)
+public class User_ {
+	public static volatile SingularAttribute<User, Long> id;
+	public static volatile SingularAttribute<User, String> username;
+	public static volatile SingularAttribute<User, String> password;
+}
