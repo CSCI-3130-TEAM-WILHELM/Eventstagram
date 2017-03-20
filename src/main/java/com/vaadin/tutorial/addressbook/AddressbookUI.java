0d2af62baed52130c12f5f740e83394e37dfbaad
Greydesk <rod.naugler@gmail.com>
2017-03-19 23:19:12 -0300

    Took out persistance items and seems like classes are working better - Rod and Darius

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -165,7 +165,7 @@ public class AddressbookUI extends UI {
     }
 
     private void refreshEvents(String stringFilter) {
-        eventList.setContainerDataSource(new BeanItemContainer<Event>(
+        eventList.setContainerDataSource(new BeanItemContainer<>(
         		Event.class, eventservice.findAll(stringFilter)));
         eventForm.setVisible(false);
         profilePageUI.setVisible(false);
