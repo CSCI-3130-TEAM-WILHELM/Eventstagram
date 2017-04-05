e109de3f19ebdd512bda90af93aac8bc804dd791
James <James@T100Laptop>
2017-04-05 11:32:44 -0300

    Database integration for Event class is complete.

--- src/main/java/com/vaadin/tutorial/eventstagram/backend/OurEventService.java
+++ src/main/java/com/vaadin/tutorial/eventstagram/backend/OurEventService.java
@@ -3,7 +3,6 @@ package com.vaadin.tutorial.eventstagram.backend;
 import org.apache.commons.beanutils.BeanUtils;
 
 //import com.vaadin.tutorial.eventstagram.backend.Event;
-
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
